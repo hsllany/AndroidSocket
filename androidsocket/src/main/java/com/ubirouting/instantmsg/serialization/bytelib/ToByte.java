a65bad87d23773c19221623223ecdc0f015523ec
hsllany <hsllany@163.com>
2016-07-20 14:28:06 +0800

    add bytelib

---
+++ androidsocket/src/main/java/com/ubirouting/instantmsg/serialization/bytelib/ToByte.java
@@ -0,0 +1,17 @@
+package com.ubirouting.instantmsg.serialization.bytelib;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ToByte {
+
+    int LAST = Integer.MAX_VALUE;
+
+    int order() default -1;
+
+    String description() default "null";
+}
---
+++ androidsocket/src/main/java/com/ubirouting/instantmsg/serialization/bytelib/Utils.java
@@ -0,0 +1,21 @@
+package com.ubirouting.instantmsg.serialization.bytelib;
+
+import java.lang.reflect.Method;
+
+class Utils {
+    private Utils() {
+    }
+
+    ;
+
+    static Method getAccessibleMethod(String methodName, Class<?> clazz) {
+        try {
+            Method ms = clazz.getDeclaredMethod(methodName);
+            ms.setAccessible(true);
+            return ms;
+        } catch (NoSuchMethodException | SecurityException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+}
